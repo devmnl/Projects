4fc0c91669bb962d0620ce08f01f3c5d40e1ad56
devmnl <dev.mnlfranco@gmail.com>
2024-09-20 10:47:39 -0300

    up

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,12 +1,27 @@
-self.addEventListener('install', async (event) => {
-  const CACHE_NAME = 'pwabuilder-page';
-  const offlineFallbackPage = '/index.html'; // Use um caminho correto
+// This is the "Offline copy of pages" service worker
 
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+const offlineFallbackPage = "/index.html";
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll([offlineFallbackPage]))
-      .catch(error => {
-        console.error('Erro ao adicionar ao cache:', error);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
