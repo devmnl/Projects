f417e327d37bfc8067fe0de093deeb97db22836f
devmnl <dev.mnlfranco@gmail.com>
2024-09-20 10:03:13 -0300

    up

--- pwabuilder-adv-sw.js
+++ pwabuilder-adv-sw.js
@@ -1,3 +1,23 @@
 
-  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-  precacheAndRoute([{"revision":"874b657f7148af98691dad1b3bd8f1dc","url":"aboutme.css"},{"revision":"6a2d20adacf5153dd8a3a84dd1bf1e31","url":"calcular-diametro.html"},{"revision":"14b9cb5f964c66774b8ee7efe6699850","url":"calcular-metragem.html"},{"revision":"38a62629891efa2fb190b0e9796eabf5","url":"index.html"},{"revision":"4d0b7b9030aac4a81f414cddafc68e59","url":"pagina-de-avisos.html"},{"revision":"ac0414460183e888a48b5b21d791c2cd","url":"service-worker.js"},{"revision":"af5cd1b56e5853161916a4529f39b57d","url":"sobre-mim.html"},{"revision":"0ff3fff12de7f636f356b5a9bb7b0f0b","url":"style-avisos.css"},{"revision":"4f74af2e3b0e881a4906bc79201e3eb7","url":"style-sentidos.css"},{"revision":"2ba2be5f83d4783ae9e6f33f7126d8dd","url":"style.css"},{"revision":"3279e101c4400426b13078f24f533db1","url":"tabela-sentidos.html"},{"revision":"09dda91daebb44311515416c12bb4847","url":"vemax.css"},{"revision":"b92be6fe8fd36d54739bdd292e0f2024","url":"vemax.html"}]);
+// pwabuilder-adv-sw.js
+const CACHE_NAME = 'your-cache-name';
+const offlineFallbackPage = 'index.html'; // Ou outra página
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        offlineFallbackPage,
+        // outros arquivos que você precisa
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(offlineFallbackPage);
+    })
+  );
+});
