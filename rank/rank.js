3e9aa44772d70a782afc68debbadca8cba85ef20
devmnl <dev.mnlfranco@gmail.com>
2024-09-30 21:12:43 -0300

    up

--- rank/rank.js
+++ rank/rank.js
@@ -2,41 +2,41 @@
 
 // Dados fictícios organizados por operadores para a Máquina 1
 const dataMachine1 = [
-    { name: 'MARCIO', production: 0 },  // Produção de Marcio
-    { name: 'MANOEL', production: 0 },   // Produção de Manoel
-    { name: 'WESLEY', production: 0 },   // Produção de Wesley
-    { name: 'LUIZ', production: 0 }      // Produção de Luiz
+    { name: 'MARCIO', production: 8 },
+    { name: 'MANOEL', production: 3 },
+    { name: 'WESLEY', production: 6 },
+    { name: 'LUIZ', production: 7 }
 ];
 
 // Dados fictícios organizados por operadores para a Máquina 2
 const dataMachine2 = [
-    { name: 'RENÊ', production: 0 },     // Produção de Renê
-    { name: 'VITOR', production: 0 },   // Produção de Vitor
-    { name: 'DOUGLAS', production: 0 },  // Produção de Douglas
-    { name: 'ERIC', production: 0 }      // Produção de Eric
+    { name: 'RENÊ', production: 0 },
+    { name: 'VITOR', production: 0 },
+    { name: 'DOUGLAS', production: 0 },
+    { name: 'ERIC', production: 0 }
 ];
 
 const employeesMachine1 = [
-    { name: 'MARCIO', image: '/images/marcio.jpeg', color: 'rgba(255, 99, 132, 0.6)' },
-    { name: 'MANOEL', image: '/images/manoel.jpeg', color: 'rgba(54, 162, 235, 0.6)' },
-    { name: 'WESLEY', image: '/images/wesley.jpeg', color: 'rgba(255, 159, 64, 0.6)' },
-    { name: 'LUIZ', image: '/images/luiz.jpeg', color: 'rgba(153, 102, 255, 0.6)' }
+    { name: 'MARCIO', image: '/images/marcio.jpeg', color: 'rgba(255, 99, 132, 0.6)', description: 'Dia' },
+    { name: 'MANOEL', image: '/images/manoel.jpeg', color: 'rgba(54, 162, 235, 0.6)' , description: 'Noite'},
+    { name: 'WESLEY', image: '/images/wesley.jpeg', color: 'rgba(255, 159, 64, 0.6)' , description: 'Noite'},
+    { name: 'LUIZ', image: '/images/luiz.jpeg', color: 'rgba(153, 102, 255, 0.6)' , description: 'Dia'}
 ];
 
 const employeesMachine2 = [
-    { name: 'RENÊ', image: '/images/rene.jpeg', color: 'rgba(153, 102, 255, 0.6)' },
-    { name: 'VITOR', image: '/images/default-user.png', color: 'rgba(255, 99, 132, 0.6)' },
-    { name: 'DOUGLAS', image: '/images/default-user.png', color: 'rgba(54, 162, 235, 0.6)' },
-    { name: 'ERIC', image: '/images/eric.jpeg', color: 'rgba(255, 159, 64, 0.6)' }
+    { name: 'RENÊ', image: '/images/rene.jpeg', color: 'rgba(153, 102, 255, 0.6)' , description: 'Dia'},
+    { name: 'VITOR', image: '/images/default-user.png', color: 'rgba(255, 99, 132, 0.6)' , description: 'Dia'},
+    { name: 'DOUGLAS', image: '/images/default-user.png', color: 'rgba(54, 162, 235, 0.6)', description: 'Noite' },
+    { name: 'ERIC', image: '/images/eric.jpeg', color: 'rgba(255, 159, 64, 0.6)' , description: 'Noite'}
 ];
 
 // Função de configuração do gráfico com cores personalizadas
-const config = (data, employees) => ({
+const createChartConfig = (data, employees) => ({
     type: 'bar',
     data: {
         labels: employees.map(e => e.name),
         datasets: [{
-            data: data.map(e => e.production),  // Acesse a produção corretamente
+            data: data.map(e => e.production),
             backgroundColor: employees.map(e => e.color),
             borderColor: employees.map(e => e.color.replace('0.6', '1')),
             borderWidth: 1
@@ -45,7 +45,7 @@ const config = (data, employees) => ({
     options: {
         plugins: {
             legend: {
-                display: false // Oculta a legenda
+                display: false
             }
         },
         scales: {
@@ -54,7 +54,7 @@ const config = (data, employees) => ({
             },
             x: {
                 title: {
-                    display: false // Oculta o título do eixo X
+                    display: false
                 }
             }
         }
@@ -62,9 +62,7 @@ const config = (data, employees) => ({
 });
 
 // Função para calcular o total de produção
-const calculateTotalProduction = (data) => {
-    return data.reduce((total, operator) => total + operator.production, 0);
-};
+const calculateTotalProduction = (data) => data.reduce((total, operator) => total + operator.production, 0);
 
 // Função para exibir o total de produção acima do gráfico
 const displayTotalProduction = (total, elementId) => {
@@ -73,26 +71,20 @@ const displayTotalProduction = (total, elementId) => {
 };
 
 // Criação dos gráficos para cada máquina
-const ctx1 = document.getElementById('chartMachine1').getContext('2d');
-const chartMachine1 = new Chart(ctx1, config(dataMachine1, employeesMachine1));
+const createCharts = () => {
+    const ctx1 = document.getElementById('chartMachine1').getContext('2d');
+    new Chart(ctx1, createChartConfig(dataMachine1, employeesMachine1));
 
-const ctx2 = document.getElementById('chartMachine2').getContext('2d');
-const chartMachine2 = new Chart(ctx2, config(dataMachine2, employeesMachine2));
-
-// Calcular e exibir o total de produção de cada máquina
-const totalMachine1 = calculateTotalProduction(dataMachine1);
-const totalMachine2 = calculateTotalProduction(dataMachine2);
-
-displayTotalProduction(totalMachine1, 'totalMachine1');
-displayTotalProduction(totalMachine2, 'totalMachine2');
+    const ctx2 = document.getElementById('chartMachine2').getContext('2d');
+    new Chart(ctx2, createChartConfig(dataMachine2, employeesMachine2));
+};
 
 // Função para formatar os valores com a unidade correta
 const formatValueWithUnit = (value) => {
     if (value >= 1) {
-        return `${value.toFixed(2).toLocaleString()} t`; // Exibe em toneladas com 2 casas decimais para valores maiores ou iguais a 1 tonelada
+        return `${value.toFixed(2).toLocaleString()} t`;
     } else {
-        const kg = value * 1000; // Converte toneladas para kg
-        return `${kg.toLocaleString()} kg`; // Exibe em kg para valores menores que 1 tonelada
+        return `${(value * 1000).toLocaleString()} kg`;
     }
 };
 
@@ -100,17 +92,18 @@ const formatValueWithUnit = (value) => {
 const generateRanking = (data, employees, elementId) => {
     const sortedData = data.map((value, index) => ({
         ...employees[index],
-        value: value.production // Acesse a produção corretamente
+        value: value.production
     })).sort((a, b) => b.value - a.value);
 
     const rankingElement = document.getElementById(elementId);
     rankingElement.innerHTML = '';
+
     sortedData.forEach((item, index) => {
         const div = document.createElement('div');
-
+        
         // Cria a imagem do funcionário
         const img = document.createElement('img');
-        img.src = item.image; // Usa o caminho completo da imagem fornecido
+        img.src = item.image;
         img.alt = item.name;
 
         // Formata o valor com a unidade correta (t ou kg)
@@ -118,24 +111,62 @@ const generateRanking = (data, employees, elementId) => {
 
         // Cria o texto do funcionário com a colocação e cor correspondente
         const text = document.createElement('span');
-        if (item.value === 0) {
-            text.innerHTML = `${item.name}: <span style="color: gray; font-weight: bold;">-</span>`;
-        } else {
-            text.innerHTML = `${index + 1}º - ${item.name}: <span style="color: gray; font-weight: bold;">${formattedValue}</span>`;
-        }
-        text.style.color = item.color.replace('0.6', '1'); // Aplica a cor correspondente
+        text.innerHTML = item.value === 0
+            ? `${item.name}: <span style="color: gray; font-weight: bold;">-</span>`
+            : `${index + 1}º - ${item.name}: <span style="color: gray; font-weight: bold;">${formattedValue}</span>`;
+        text.style.color = item.color.replace('0.6', '1');
 
         // Adiciona a imagem e o texto ao div
         div.appendChild(img);
         div.appendChild(text);
 
+        // Adiciona evento de clique na imagem
+        img.addEventListener('click', () => showEmployeeInfo(item));
         rankingElement.appendChild(div);
     });
 };
 
 // Gerar o ranking para cada máquina
-generateRanking(dataMachine1, employeesMachine1, 'rankingMachine1');
-generateRanking(dataMachine2, employeesMachine2, 'rankingMachine2');
+const generateRankings = () => {
+    generateRanking(dataMachine1, employeesMachine1, 'rankingMachine1');
+    generateRanking(dataMachine2, employeesMachine2, 'rankingMachine2');
+};
+
+// Função para mostrar o modal com informações do funcionário
+const showEmployeeInfo = (employee) => {
+    const modal = document.getElementById('employeeModal');
+    const modalImage = document.getElementById('modalImage');
+    const modalName = document.getElementById('modalName');
+    const modalDescription = document.getElementById('modalDescription');
+
+    modalImage.src = employee.image;
+    modalName.textContent = employee.name;
+    modalDescription.textContent = `Turno: ${employee.description} `
+
+    modal.style.display = 'block';
+};
+
+// Fechar modal ao clicar no botão de fechar
+const closeModal = () => {
+    const modal = document.getElementById('employeeModal');
+    modal.style.display = 'none';
+};
+
+// Adiciona evento para fechar o modal ao clicar no 'x'
+const closeButton = document.querySelector('.close');
+closeButton.addEventListener('click', closeModal);
+
+// Fecha o modal se o usuário clicar fora do conteúdo do modal
+window.onclick = function(event) {
+    const modal = document.getElementById('employeeModal');
+    if (event.target === modal) {
+        closeModal();
+    }
+};
+
+// Inicializar gráficos e rankings
+createCharts();
+generateRankings();
 
 
 
